50b438ac63999b823b51056ee088f66d15acc3c4
Steve Dower <steve.dower@microsoft.com>
2016-08-23 16:59:15 -0700

    [WIP] Starts support for Open Folder This won't ever be fully supported on Dev14, but we can load the reference assembly there which will simplify initial development. The reference assembly will eventually go away and the project disabled on Dev14 builds (unless we completely disable Dev14 builds first).

---
+++ Python/Product/Workspace/PythonLanguageServiceProviderFactory.cs
@@ -0,0 +1,34 @@
+﻿// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Workspace;
+using Microsoft.VisualStudio.Workspace.Intellisense;
+
+namespace Microsoft.PythonTools.Workspace {
+    [ExportFileContextProvider(ProviderType, ProviderPriority.Normal, PythonFileContext.ContextType)]
+    class PythonLanguageServiceProviderFactory : IWorkspaceProviderFactory<ILanguageServiceProvider> {
+        public const string ProviderType = "1B8A30A0-A9C3-40F2-999A-DBB9F8571AA9";
+
+        public ILanguageServiceProvider CreateProvider(IWorkspace workspaceContext) {
+            return new PythonLanguageServiceProvider(workspaceContext);
+        }
+    }
+}
---
+++ Python/Product/Workspace/PythonSymbolScanner.cs
@@ -0,0 +1,43 @@
+﻿// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Workspace;
+using Microsoft.VisualStudio.Workspace.Indexing;
+
+namespace Microsoft.PythonTools.Workspace {
+    [ExportFileScanner(ProviderType, "Python", new[] { ".py", ".pyw", ".pyx" }, new[] { typeof(IReadOnlyCollection<SymbolDefinition>) })]
+    class PythonSymbolScanner : IWorkspaceProviderFactory<IFileScanner>, IFileScanner {
+        public const string ProviderType = "3E6F8CCC-AEE9-458B-B74B-A83D044A1468";
+
+        public IFileScanner CreateProvider(IWorkspace workspaceContext) {
+            return this;
+        }
+
+        public async Task<T> ScanContentAsync<T>(string filePath, CancellationToken cancellationToken) where T : class {
+            var res = new List<SymbolDefinition>();
+
+            // TODO: Parse symbols
+
+            return res as T;
+        }
+    }
+}
